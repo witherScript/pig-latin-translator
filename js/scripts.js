b76d04506e08bdfb5fff02bb50b4d6a3dc5ff6bb
Genesis Scott <chandlerscott123@gmail.com>
2023-06-25 18:05:55 -0500

    at functionality for multi-consonant including qu words

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
 function pigLatinTranslate(str){
   const vowels = ['a','e','i','o','u'];
   let strArr = str.split(' ');
-    
+
   strArr.forEach(function(word, index){
-    if(vowels.join().includes(word[0])){
-      strArr[index]+='way';
+    if(vowels.includes(word[0])){
+      strArr[index] += 'way';
     }
     else {
       let stoppingPoint = 0;
       for(let i = 0; i < word.length; i++) {
         if(vowels.includes(word[i])) {
           stoppingPoint = i;
+          // Special case for "qu"
+          if(word[i-1] === 'q' && word[i] === 'u') {
+            stoppingPoint++;
+          }
           break;
         }
       }
       strArr[index] = word.slice(stoppingPoint) + word.slice(0, stoppingPoint) + 'ay';
     }
-    });
-    str = strArr.join(' ');
-    return str;
+  });
+
+  return strArr.join(' ');
 }
